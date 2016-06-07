c370d670ed17e74131d2012243a523d5998d24ae
jbaclig <jon@baclig.com>
2016-06-06 22:37:41 -0700

    add 'posts' factory service

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var app = angular.module('flapperNews',[]);
 
-app.controller('MainCtrl',['$scope',function($scope){
+app.controller('MainCtrl',['$scope','posts',function($scope){
   $scope.test = 'Hello world!';
-
-  $scope.posts = [
+  $scope.posts = posts.posts;
+  /*$scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
     {title: 'post 3', upvotes: 15},
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
-  ];
+  ];*/
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
@@ -26,4 +26,11 @@ app.controller('MainCtrl',['$scope',function($scope){
     post.upvotes +=1;
   };
 
+}]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
 }]);
