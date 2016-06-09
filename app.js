7bc1e870b695d054dcb8dadc4023204035ecbc9c
jbaclig <jon@baclig.com>
2016-06-08 21:21:15 -0700

    add templates

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-var app = angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',['ui.router']);
 
-app.controller('MainCtrl',['$scope','posts',function($scope){
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider,$urlRouterProvider) {
+    $stateProvider
+      .state('home',{
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+      $stateProvider
+        .state('posts',{
+          url: '/posts{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
+        });
+
+    $urlRouterProvider.otherwise('home');
+  }
+]);
+
+app.controller('MainCtrl',['$scope','posts',function($scope,posts){
   $scope.test = 'Hello world!';
   $scope.posts = posts.posts;
-  /*$scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];*/
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Gread idea but everything is wong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link='';
@@ -27,6 +46,15 @@ app.controller('MainCtrl',['$scope','posts',function($scope){
   };
 
 }]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope,$stateParams,posts){
+    $scope.post = posts.posts[$stateParams.id];
+  }
+]);
 
 app.factory('posts', [function(){
   var o = {
