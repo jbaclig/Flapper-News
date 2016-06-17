297ac643ba27f4a557cbc86f569f15ab96870f25
Jon Baclig <jon@baclig.com>
2016-06-17 12:14:15 -0700

    add Passport for user authentication

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -10,6 +10,10 @@ require('./models/Posts');
 require('./models/Comments');
 mongoose.connect('mongodb://localhost:27017/news');
 
+var passport = require('passport');
+require('./models/Users');
+require('./config/passport');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -30,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 var express = require('express');
-var router = express.Router();
+var passpot = require('passport');
+var jwt = require('express-jwt');
 
+var router = express.Router();
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -97,5 +100,43 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
     res.json(comment);
   });
 });
+
+/* authenticate users */
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+/* user login */
+router.post('./login',function(req, res, next){
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 module.exports = router;
