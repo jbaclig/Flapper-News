69970071d2a232381529d79858a5928a0420746c
Jon Baclig <jon@baclig.com>
2016-06-17 13:49:16 -0700

    add angular service for logging in

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -22,8 +22,9 @@ router.get('/posts',function(req,res,next){
 });
 
 /* POST a post */
-router.post('/posts',function(req,res,next){
+router.post('/posts', auth, function(req,res,next){
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err,post){
     if(err){ return next(err); }
@@ -67,7 +68,7 @@ router.get('/posts/:post', function(req, res, next) {
 });
 
 /* upvote a post */
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -76,9 +77,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 });
 
 /* comments router */
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
@@ -93,7 +95,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
 });
 
 /* upvote a comment */
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
